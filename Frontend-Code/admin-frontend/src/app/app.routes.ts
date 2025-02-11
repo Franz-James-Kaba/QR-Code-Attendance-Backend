8364fd8160238abb2bc6c6146307b6d91e488561
DNA_Q <david.quaye@amalitech.com>
2025-02-11 14:07:37 +0000

    Remove VSCode configuration files and add input field component with validation

--- Frontend-Code/admin-frontend/src/app/app.routes.ts
+++ Frontend-Code/admin-frontend/src/app/app.routes.ts
@@ -1,5 +1,25 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './layouts/auth-layout/layout.component';
 
 export const routes: Routes = [
-  // Add your routes here
+  {
+    path: 'auth',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+      },
+      {
+        path: 'login',
+        loadComponent: () => import('./features/auth/pages/login/login.component').then(m => m.LoginComponent)
+      }
+    ]
+  },
+  {
+    path: '',      // Redirect root to auth/login
+    redirectTo: 'auth/login',
+    pathMatch: 'full'
+  }
 ];
