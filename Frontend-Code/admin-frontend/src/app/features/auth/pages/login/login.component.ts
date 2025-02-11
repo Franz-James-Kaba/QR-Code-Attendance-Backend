70bf4b268ff365f40593df40ca6a9740da4d0d3d
DNA_Q <david.quaye@amalitech.com>
2025-02-11 16:18:44 +0000

    Add button and input field components with styles, implement login form with validation

--- Frontend-Code/admin-frontend/src/app/features/auth/pages/login/login.component.ts
+++ Frontend-Code/admin-frontend/src/app/features/auth/pages/login/login.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { InputFieldComponent } from '../../../../shared/components/input-field/input-field.component';
+import { ButtonComponent } from '../../../../shared/components/button/button.component';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [InputFieldComponent, ReactiveFormsModule, ButtonComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
 
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+
+  constructor(private readonly fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  ngOnInit(): void {
+    // Do something
+
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      console.log('Form submitted:', this.loginForm.value);
+    }
+  }
 }
