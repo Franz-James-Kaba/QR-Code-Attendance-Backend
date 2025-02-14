6b2c5ac32209a1b186e7b72e4646fe22d9682acc
Julius Agbame <julius.agbame@amalitech.com>
2025-02-14 08:50:03 +0000

    Added the delete method

---
+++ Backend-Code/attendance-system/src/main/java/com/example/attendance_system/exceptions/UserNotFoundException.java
@@ -0,0 +1,4 @@
+package com.example.attendance_system.exceptions;
+
+public class UserNotFoundException {
+}
--- Backend-Code/attendance-system/src/main/java/com/example/attendance_system/user/UserService.java
+++ Backend-Code/attendance-system/src/main/java/com/example/attendance_system/user/UserService.java
@@ -2,10 +2,12 @@ package com.example.attendance_system.user;
 
 import com.example.attendance_system.config.JWTService;
 import com.example.attendance_system.email.EmailService;
+import com.example.attendance_system.exceptions.UserNotFoundException;
 import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -120,5 +122,10 @@ public class UserService {
         user.setLastName(request.getLastName());
         user.setEmail(request.getEmail());
         return userRepository.save(user);
+    }
+
+    public void deleteUser(Long userId) {
+        var user = userRepository.findById(userId).orElseThrow(() -> new UserNotFoundException("User not found"));
+        userRepository.delete(user);
     }
 }
