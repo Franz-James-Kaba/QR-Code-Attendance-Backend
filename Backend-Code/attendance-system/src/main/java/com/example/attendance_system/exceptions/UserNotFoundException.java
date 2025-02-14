bfcc160a8593275b171b23fdef702d0caaf0b269
Julius Agbame <julius.agbame@amalitech.com>
2025-02-14 08:57:02 +0000

    Added some exception handling class

--- Backend-Code/attendance-system/src/main/java/com/example/attendance_system/exceptions/UserNotFoundException.java
+++ Backend-Code/attendance-system/src/main/java/com/example/attendance_system/exceptions/UserNotFoundException.java
@@ -1,4 +1,11 @@
 package com.example.attendance_system.exceptions;
 
-public class UserNotFoundException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
 }
