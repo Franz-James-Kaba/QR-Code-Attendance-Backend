9fe985f2c12e11cb3a848ffab4ad7f01735cce61
Bernard Essilfie <bernard.essilfie@amalitech.com>
2025-03-18 10:47:00 +0000

    clean up environment

---
+++ Backend-Code/attendance-system/src/test/java/com/example/attendance_system/user/UserServiceTest.java
@@ -0,0 +1,12 @@
+package com.example.attendance_system.user;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+    @Test
+    void deleteUser() {
+    }
+}
