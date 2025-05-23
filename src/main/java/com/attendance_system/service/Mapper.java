818a0fafce0be4f04e705553b59e9bbb1c39b2fc
Bernard Essilfie <bernard.essilfie@amalitech.com>
2025-05-23 09:47:41 +0000

    delete SessionDTO

--- src/main/java/com/attendance_system/service/Mapper.java
+++ src/main/java/com/attendance_system/service/Mapper.java
@@ -2,22 +2,10 @@ package com.attendance_system.service;
 
 import com.attendance_system.dto.CustomAttendance;
 import com.attendance_system.model.Attendance;
-import com.attendance_system.model.Session;
 import org.springframework.stereotype.Service;
 
 @Service
 public class Mapper {
-
-    public SessionDTO toSessionDTO(Session session) {
-        return SessionDTO.builder()
-                .id(session.getId())
-                .name(session.getName())
-                .sessionCode(session.getSessionCode())
-                .startTime(session.getStartTime())
-                .endTime(session.getEndTime())
-                .active(session.isActive())
-                .build();
-    }
 
     public CustomAttendance toAttendanceDTO(Attendance attendance) {
         return CustomAttendance.builder()
